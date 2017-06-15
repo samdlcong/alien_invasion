5583f45278f12c982e1b7f213bd8079873e02cd1
samdlcong <samnew@126.com>
2017-06-15 14:01:47 +0800

    update alien_invasion

--- ship.py
+++ ship.py
@@ -2,9 +2,10 @@ import pygame
 
 class Ship():
 
-    def __init__(self,screen):
+    def __init__(self, ai_settings, screen):
         """初始化飞串并设置其初始位置"""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         #加载飞船图像并获取其外接矩形
         self.image = pygame.image.load('images/ship2.bmp')
@@ -14,6 +15,9 @@ class Ship():
         #将每艘新飞船放在屏幕底部中央
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+
+        #在飞船的属性center中存储小数值
+        self.center = float(self.rect.centerx)
 
         #移动标志
         self.moving_right = False
@@ -25,7 +29,10 @@ class Ship():
 
     def update(self):
         """根据移动标志调整飞船的位置"""
-        if self.moving_right:
-            self.rect.centerx += 1
-        if self.moving_left:
-            self.rect.centerx -= 1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+        
+        # 根据self.center 更新rect对象 自动忽略小数
+        self.rect.centerx = self.center
