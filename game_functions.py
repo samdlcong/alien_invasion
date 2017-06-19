c23078e7fdac9c643ff5a6ec6dce8751d5123328
samdlcong <samnew@126.com>
2017-06-19 13:22:37 +0800

    bullets aliens collision

--- game_functions.py
+++ game_functions.py
@@ -54,10 +54,13 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
     #让最近绘制的屏幕可见
     pygame.display.flip()
 
-def update_bullets(bullets):
+def update_bullets(aliens, bullets):
     """更新子弹的位置，并删除已消失的子弹"""
     #更新子弹的位置
     bullets.update()
+    #检查是否有子弹击中了外星人
+    #如果是这样，就删除相应的子弹和外星人
+    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
     #删除已消失的子弹
     for bullet in bullets.copy():
@@ -99,6 +102,21 @@ def get_number_rows(ai_settings, ship_height, alien_height):
     number_rows = int(availabe_space_y / (2 * alien_height))
     return number_rows
 
-def update_aliens(aliens):
+def update_aliens(ai_settings, aliens):
     """更新外星人群中所有外星人的位置"""
+    check_fleet_edges(ai_settings, aliens)
     aliens.update()
+
+def check_fleet_edges(ai_settings, aliens):
+    """有外星人到达边缘时采取相应的措施"""
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+
+def change_fleet_direction(ai_settings, aliens):
+    """将整群外星人下移，并改变它们的方向"""
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
+
