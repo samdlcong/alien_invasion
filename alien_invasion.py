97277876a25261d1715dd24571ec58400ed668cc
samdlcong <samnew@126.com>
2017-06-20 08:26:24 +0800

    add play button

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group 
 from game_stats import GameStats 
+from button import Button
 
 def run_game():
     #初始化游戏并创建一个屏幕对象
@@ -12,6 +13,10 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    #创建Play按钮
+    play_button = Button(ai_settings, screen, "Play")
+
 
     #创建一个用于存储游戏统计信息的实例
     stats = GameStats(ai_settings)
@@ -41,7 +46,7 @@ def run_game():
             # #print(len(bullets))
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats,ship, aliens, bullets, play_button)
 
 
 run_game()
