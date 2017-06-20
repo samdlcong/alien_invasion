0f3f9d9ed0e2598d2e506998f877d73e75d8e725
samdlcong <samnew@126.com>
2017-06-20 14:12:42 +0800

    add speedup scale

--- game_functions.py
+++ game_functions.py
@@ -83,6 +83,7 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
     if len(aliens) == 0:
         #删除现有的子弹并新建一群外星人
         bullets.empty()
+        ai_settings.increase_speed()
         create_fleet(ai_settings, screen, ship, aliens)
 
 
@@ -178,6 +179,8 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
     """在玩家单击Play按钮时开始新游戏"""
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
+        #重置游戏设置
+        ai_settings.initialize_dynamic_settings()
         start_game(ai_settings, screen, stats, ship, aliens, bullets)
 
 
--- settings.py
+++ settings.py
@@ -8,20 +8,39 @@ class Settings():
         self.bg_color = (230, 230, 230)
 
         # 飞船设置
-        self.ship_speed_factor = 1.5
+        #self.ship_speed_factor = 1.5
         self.ship_limit = 3
         
         # 子弹设置
-        self.bullet_speed_factor = 1
+        #self.bullet_speed_factor = 1
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 255, 0, 0
         self.bullets_allowed = 5
 
         #外星人设置
-        self.alien_speed_factor = 1
+        #self.alien_speed_factor = 1
         self.fleet_drop_speed = 20
         # fleet_direction为1表示向右移，为-1表示向左移
+        #self.fleet_direction = 1
+
+        # 以什么样的速度加快游戏节奏
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """初始化随游戏进行而变化的设置"""
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1
+
+        # fleet_direction为1表示向右，为-1表示向左
         self.fleet_direction = 1
 
-        
+    def increase_speed(self):
+        """提高速度设置"""
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
+
